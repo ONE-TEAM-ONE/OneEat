bde44c6c171fb925d414417258d3d823c13d2b41
권현준 <bonheur9813@naver.com>
2024-11-08 09:24:17 +0900

    feat : Auditing 기능 설정 #1

---
+++ src/main/java/com/sparta/oneeat/common/entity/BaseEntity.java
@@ -0,0 +1,42 @@
+package com.sparta.oneeat.common.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    // 생성시간
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    // 수정시간
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    // 삭제시간
+    private LocalDateTime deletedAt;
+
+    // 생성자
+    @CreatedBy
+    @Column(updatable = false)
+    private Long createdBy;
+
+    // 수정자
+    @LastModifiedBy
+    private Long updatedBy;
+
+    // 삭제자
+    private Long deletedBy;
+
+}
