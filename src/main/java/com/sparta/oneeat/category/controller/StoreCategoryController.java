c8e78e032ad68936820cdc18221d8185f982b2f6
yurim <y1551463@gmail.com>
2024-11-15 17:57:13 +0900

    feat : 카테고리 수정 #46

--- src/main/java/com/sparta/oneeat/category/controller/StoreCategoryController.java
+++ src/main/java/com/sparta/oneeat/category/controller/StoreCategoryController.java
@@ -1,19 +1,23 @@
 package com.sparta.oneeat.category.controller;
 
 import com.sparta.oneeat.auth.service.UserDetailsImpl;
-import com.sparta.oneeat.common.response.BaseResponseBody;
 import com.sparta.oneeat.category.dto.CreateCategoryReqDto;
+import com.sparta.oneeat.category.dto.UpdateCategoryReqDto;
 import com.sparta.oneeat.category.service.StoreCategoryService;
+import com.sparta.oneeat.common.response.BaseResponseBody;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,5 +46,21 @@ public class StoreCategoryController {
         log.info("category : {}", createCategoryReqDto.getCategory());
 
         return ResponseEntity.status(201).body(BaseResponseBody.of(0, storeCategoryService.createCategory(userDetails.getId(), createCategoryReqDto)));
+    }
+
+    @Operation(summary = "가게 카테고리 수정", description = "가게 카테고리를 수정합니다.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "카테고리 수정 성공"),
+        @ApiResponse(responseCode = "500", description = "카테고리 수정 실패")
+    })
+    @PutMapping("/{categoryId}")
+    public ResponseEntity<? extends BaseResponseBody> updateCategory(
+        @AuthenticationPrincipal UserDetailsImpl userDetails,
+        @RequestBody UpdateCategoryReqDto requestDto,
+        @PathVariable UUID categoryId) {
+
+        storeCategoryService.updateCategory(userDetails.getUser(), requestDto, categoryId);
+
+        return ResponseEntity.status(200).body(BaseResponseBody.of(0, null));
     }
 }
--- src/main/java/com/sparta/oneeat/category/dto/UpdateCategoryReqDto.java
+++ src/main/java/com/sparta/oneeat/category/dto/UpdateCategoryReqDto.java
@@ -1,10 +1,12 @@
-package com.sparta.oneeat.store.dto.request;
+package com.sparta.oneeat.category.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
-public class CategoryRequestDto {
+@Getter
+@Setter
+public class UpdateCategoryReqDto {
 
     @NotBlank(message = "카테고리명은 필수 입력입니다.")
     private String categoryName;
