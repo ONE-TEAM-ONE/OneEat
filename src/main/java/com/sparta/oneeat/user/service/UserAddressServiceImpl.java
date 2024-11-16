60d5f359ef2ee2dbc13be940ce2abab8ed7244c0
JISU-YANG <jisuyang@egoist.im>
2024-11-16 14:51:47 +0900

    refacter : 검증 로직 분리 및 로그 추가 #62

--- src/main/java/com/sparta/oneeat/user/service/UserAddressServiceImpl.java
+++ src/main/java/com/sparta/oneeat/user/service/UserAddressServiceImpl.java
@@ -27,35 +27,28 @@ public class UserAddressServiceImpl implements UserAddressService {
     @Transactional
     public UUID creatAddress(User user, String address) {
 
-        // 회원 아이디 가져와서 유저 검증
-        userRepository.findById(user.getId()).orElseThrow(()->
-                new CustomException(ExceptionType.USER_NOT_EXIST)
-        );
+        this.validateUserExist(user.getId());
 
-        // 이미 같은 주소가 등록되어있는지 확인
-        if(userAddressRepository.findByUserIdAndAddress(user.getId(), address).isPresent()){
+        if (userAddressRepository.findByUserIdAndAddress(user.getId(), address).isPresent()) {
+            log.warn("이미 같은 주소가 회원의 주소록에 존재합니다.");
             throw new CustomException(ExceptionType.USER_EXIST_ADDRESS);
         }
 
-
         UserAddress userAddress = new UserAddress(user, address);
         userAddressRepository.save(userAddress);
+        log.info("주소가 추가되었습니다. Address: {}", userAddress.getAddress());
 
         return userAddress.getId();
     }
 
     @Override
     public List<AddressResponseDto> selectAddressList(Long userId) {
-        // FK로 사용되는 유저가 존재하는지
-        userRepository.findById(userId).orElseThrow(()->
-                new CustomException(ExceptionType.USER_NOT_EXIST)
-        );
 
-        List<UserAddress> userAddressList =
-                userAddressRepository.findByUserId(userId);
+        this.validateUserExist(userId);
 
-        // 등록된 주소가 없다면 예외처리
+        List<UserAddress> userAddressList = userAddressRepository.findByUserId(userId);
         if (userAddressList.isEmpty()) {
+            log.warn("회원의 주소록에 주소가 존재하지 않습니다.");
             throw new CustomException(ExceptionType.USER_NOT_EXIST_ADDRESS);
         }
 
@@ -67,53 +60,75 @@ public class UserAddressServiceImpl implements UserAddressService {
     @Override
     @Transactional
     public void modifyCurrentAddress(User user, UUID addressId) {
-        // 자신의 주소록에 주소가 있는지 확인
-        UserAddress userAddress = userAddressRepository.findByIdAndUserId(addressId, user.getId()).orElseThrow(()->
-                new CustomException(ExceptionType.USER_NOT_EXIST_ADDRESS)
-        );
 
-        // 유저의 기본 주소에 반영하기
+        this.validateUserExist(user.getId());
+
+        UserAddress userAddress = this.validateAddressExist(user.getId(), addressId);
+
         user.modifyCurrentAddress(userAddress.getAddress());
-
+        log.info("회원의 현재 주소가 변경되었습니다. CurrentAddress: {}", user.getCurrentAddress());
     }
 
     @Override
     @Transactional
     public void modifyAddress(Long userId, UUID addressId, String address) {
-        // 자신의 주소록에 주소가 있는지 확인
-        UserAddress userAddress = userAddressRepository.findByIdAndUserId(addressId, userId).orElseThrow(()->
-                new CustomException(ExceptionType.USER_NOT_EXIST_ADDRESS)
-        );
 
-        // 주소 수정
+        this.validateUserExist(userId);
+
+        UserAddress userAddress = this.validateAddressExist(userId, addressId);
+
         userAddress.modifyAddress(address);
+        log.info("주소 내용이 변경되었습니다. Address: {}", userAddress.getAddress());
     }
 
     @Override
     @Transactional
     public void softDeleteAddress(Long userId, UUID addressId) {
-        // 자신의 주소록에 주소가 있는지 확인
-        UserAddress userAddress = userAddressRepository.findByIdAndUserId(addressId, userId).orElseThrow(()->
-                new CustomException(ExceptionType.USER_NOT_EXIST_ADDRESS)
-        );
 
-        // 숨김처리
+        this.validateUserExist(userId);
+
+        UserAddress userAddress = validateAddressExist(userId, addressId);
+
         userAddress.softDelete(userId);
+        log.info("주소가 비활성화 되었습니다. DeletedAt: {}", userAddress.getDeletedAt());
     }
 
     @Override
     @Transactional
     public void hardDeleteAddress(UUID addressId) {
-        // 전체 회원 주소록에 주소가 있는지 확인
-        UserAddress userAddress = userAddressRepository.findById(addressId).orElseThrow(()->
+
+        UserAddress userAddress = userAddressRepository.findById(addressId).orElseThrow(() ->
                 new CustomException(ExceptionType.USER_NOT_EXIST_ADDRESS)
         );
-        // 숨김처리 상태인지 확인
-        if(userAddress.getDeletedAt() == null){
+        log.info("주소 정보가 확인되었습니다. Address: {}", userAddress.getAddress());
+
+
+        if (userAddress.getDeletedAt() == null) {
+            log.warn("주소가 비활성화 상태가 아닙니다.");
             throw new CustomException(ExceptionType.USER_NOT_SOFT_DELETE_ADDRESS);
         }
 
-        // 삭제 처리
         userAddressRepository.deleteById(addressId);
+        log.info("주소가 삭제되었습니다. DeletedAt: {}", userAddress.getDeletedAt());
+    }
+
+    @Transactional
+    protected User validateUserExist(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() ->
+                new CustomException(ExceptionType.USER_NOT_EXIST)
+        );
+        log.info("회원 정보가 확인되었습니다. userId: {}", userId);
+
+        return user;
+    }
+
+    @Transactional
+    protected UserAddress validateAddressExist(Long userId, UUID addressId) {
+        UserAddress userAddress = userAddressRepository.findByIdAndUserId(addressId, userId).orElseThrow(() ->
+                new CustomException(ExceptionType.USER_NOT_EXIST_ADDRESS)
+        );
+        log.info("회원의 주소록에 주소가 존재합니다. Address: {}", userAddress.getAddress());
+
+        return userAddress;
     }
 }
