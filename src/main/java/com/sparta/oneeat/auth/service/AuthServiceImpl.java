d3a8ddf1ec206a16aff3f7486c44dd76521741e1
JISU-YANG <jisuyang@egoist.im>
2024-11-16 11:41:53 +0900

    refactor : 컨트롤러에서 데이터 바인딩 #54

--- src/main/java/com/sparta/oneeat/auth/service/AuthServiceImpl.java
+++ src/main/java/com/sparta/oneeat/auth/service/AuthServiceImpl.java
@@ -1,6 +1,5 @@
 package com.sparta.oneeat.auth.service;
 
-import com.sparta.oneeat.auth.dto.SignupRequestDto;
 import com.sparta.oneeat.auth.dto.SignupResponseDto;
 import com.sparta.oneeat.common.exception.CustomException;
 import com.sparta.oneeat.common.exception.ExceptionType;
@@ -22,28 +21,25 @@ public class AuthServiceImpl implements AuthService {
 
     @Transactional
     @Override
-    public SignupResponseDto signup(SignupRequestDto requestDto) {
-        String username = requestDto.getUsername();
-        String password = requestDto.getPassword();
-        String nickname = requestDto.getNickname();
+    public SignupResponseDto signup(String username, String password, String nickname, String email, String address) {
 
         password = passwordEncoder.encode(password);
         log.info("비밀번호를 암호화 했습니다: {}", password);
 
         if (userRepository.findByName(username).isPresent()) {
-            log.warn("중복된 ID 입니다: {}", username);
+            log.warn("중복된 유저 네임 입니다: {}", username);
             throw new CustomException(ExceptionType.AUTH_DUPLICATE_USERNAME);
         }
 
         if (userRepository.findByNickname(username).isPresent()) {
-            log.warn("이미 존재하는 닉네임 입니다: {}", nickname);
+            log.warn("중복된 닉네임 입니다: {}", nickname);
             throw new CustomException(ExceptionType.AUTH_DUPLICATE_NICKNAME);
         }
 
-        User user = new User(username, password, nickname, requestDto.getEmail(), requestDto.getAddress());
+        User user = new User(username, password, nickname, email, address);
 
         userRepository.save(user);
-        log.info("회원가입이 성공적으로 되었습니다. ID: {}", user.getName());
+        log.info("회원가입이 성공적으로 되었습니다. Username: {}", user.getName());
 
         return new SignupResponseDto(user.getId());
     }
